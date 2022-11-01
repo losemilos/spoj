6e763c6278a194e923f5b4304f2d9f5e37c70a79
Los_Emilos <los.emilos.email@gmail.com>
2022-11-01 04:14:46 +0100

    add

---
+++ RNO_DOD.py
@@ -0,0 +1,10 @@
+n = int(input())
+for i in range(0, n):
+    m = int(input())
+    a = 0
+    sum = 0
+    for j in range(0, m):
+        a = int(input())
+        sum = sum + a
+    print(sum)
+    sum = 0
